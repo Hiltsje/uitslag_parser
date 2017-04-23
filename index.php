03991401119bfcbc77ff01795775a56b9ff6524e
hiltsje <hillie@alledaags.nl>
2017-04-23 12:58:36 +0200

    Add create create results table function

--- index.php
+++ index.php
@@ -5,14 +5,53 @@
  * Date: 19-04-17
  * Time: 16:46
  */
+function createResultsTable() {
+	$servername = "localhost";
+	$username = "root";
+	$password = "root";
+	$dbname = "vitesse_uitslagen";
 
-$txt_file    = file_get_contents('Zutphen V15 10-4-15.txt');
-$rows        = explode("\n", $txt_file);
+// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	else {
+		echo 'connection success';
+	}
 
-foreach($rows as $row) {
-	//nieuwe reguliere expressie nog testen.
-	$row_data = preg_match("/^\s+(\d+)\s(.{20})\s*(\d*|\d*[,]\d*)\s+(\d*|\d*.\d*)\s+(\d*)\s(.+[A-Z]{0,1})\s+(\d+[.]\d+[.]\d+)\s*(\d*[,]\d*)\s+(\d+[,]\d)\s*$/", $row, $matches);
-	//$row_data = preg_match("/^\s+(\d+)\s(.{20})\s*(\d*|\d*[,]\d*)\s+(\d*|\d*.\d*)\s+(\d*)\s(.+?[A-Z{1}]?)\s+(\d+[.]\d+[.]\d+)\s*(\d*[,]\d*)\s+(\d+[,]\d)\s*$/", $row, $matches);
+	// sql to create table
+	$sql = "CREATE TABLE `Results` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `place` int(30) NOT NULL,
+  `distance` float DEFAULT NULL,
+  `ADAP` varchar(50) DEFAULT NULL,
+  `signed` int(50) DEFAULT NULL,
+  `ring` varchar(50) DEFAULT NULL,
+  `time` varchar(50) DEFAULT NULL,
+  `speed` float DEFAULT NULL,
+  `points` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+	if ($conn->query($sql) === TRUE) {
+		echo "Table MyGuests created successfully";
+	} else {
+		echo "Error creating table: " . $conn->error;
+	}
+
+	$conn->close();
+}
+
+
+function getResults()
+{
+	$txt_file = file_get_contents('Zutphen V15 10-4-15.txt');
+	$rows = explode("\n", $txt_file);
+
+	foreach ($rows as $row) {
+		$row_data = preg_match("/^\s+(\d+)\s(.{20})\s*(\d*|\d*[,]\d*)\s+(\d*|\d*.\d*)\s+(\d*)\s(.+[A-Z]{0,1})\s+(\d+[.]\d+[.]\d+)\s*(\d*[,]\d*)\s+(\d+[,]\d)\s*$/", $row, $matches);
 
 		$info[$row]['plaats'] = $matches[1];
 		$info[$row]['naam'] = $matches[2];
@@ -25,15 +64,18 @@ foreach($rows as $row) {
 		$info[$row]['punten'] = $matches[9];
 
 
-	echo ' Plaats: ' . $info[$row]['plaats'] . '<br />';
-	echo ' Naam: ' . $info[$row]['naam'] . '<br />';
-	echo ' Afstand: ' . $info[$row]['afstand'] . '<br />';
-	echo ' AD/AP: ' . $info[$row]['adap'] . '<br />';
-	echo ' Getekend: ' . $info[$row]['getekend'] . '<br />';
-	echo ' Ring: ' . $info[$row]['ring'] . '<br />';
-	echo ' Tijd: ' . $info[$row]['tijd'] . '<br />';
-	echo ' Snelheid: ' . $info[$row]['snelheid'] . '<br />';
-	echo ' Punten: ' . $info[$row]['punten'] . '<br />';
-	echo '<br />';
+		echo ' Plaats: ' . $info[$row]['plaats'] . '<br />';
+		echo ' Naam: ' . $info[$row]['naam'] . '<br />';
+		echo ' Afstand: ' . $info[$row]['afstand'] . '<br />';
+		echo ' AD/AP: ' . $info[$row]['adap'] . '<br />';
+		echo ' Getekend: ' . $info[$row]['getekend'] . '<br />';
+		echo ' Ring: ' . $info[$row]['ring'] . '<br />';
+		echo ' Tijd: ' . $info[$row]['tijd'] . '<br />';
+		echo ' Snelheid: ' . $info[$row]['snelheid'] . '<br />';
+		echo ' Punten: ' . $info[$row]['punten'] . '<br />';
+		echo '<br />';
 
+	}
 }
+//getResults();
+
